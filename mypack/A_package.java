50217912a1d177cec052eeceb4c72353605e6426
Rahul Singh <109950602+RahulSingh693@users.noreply.github.com>
2023-05-23 00:30:17 +0530

    Add files via upload

---
+++ mypack/A_package.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class A_package {
+
+    public int maxm(int a, int b) {
+        if (a > b)
+            return a;
+        else if(b > a)
+            return b;
+        else
+            return 0;
+    }
+}
